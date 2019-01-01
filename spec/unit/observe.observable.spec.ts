1276af6b53539f03a1ae6598c715a2d733b52d18
SalathielGenese <salathielgenese@gmail.com>
2019-01-01 08:30:41 +0000

    fills Observable API spec

--- spec/unit/observe.observable.spec.ts
+++ spec/unit/observe.observable.spec.ts
@@ -1,3 +1,8 @@
+import { Observable } from '../../src/observe';
+import { v4 as uuid } from 'uuid';
+
+
+
 describe( 'Observable', () =>
 {
 
@@ -6,12 +11,25 @@ describe( 'Observable', () =>
 
         it( 'clears registered observers', () =>
         {
-            //
+            const observer_1 = () => {};
+            const observer_2 = () => {};
+            const observer_3 = () => {};
+            const observable = new Observable();
+
+            observable.register( observer_1, observer_2, observer_3 );
+            observable.reset();
+
+            expect( observable.has( observer_1 ) ).toBe( false );
+            expect( observable.has( observer_2 ) ).toBe( false );
+            expect( observable.has( observer_3 ) ).toBe( false );
         });
 
         it( 'returns #reset() parent Observable object', () =>
         {
-            //
+            const observable = new Observable();
+            const returned = observable.reset();
+
+            expect( returned ).toBe( observable );
         });
 
     });
@@ -26,17 +44,42 @@ describe( 'Observable', () =>
 
         it( 'calls all registered observers once', () =>
         {
-            //
+            const observable = new Observable();
+            const observer_1 = jasmine.createSpy();
+            const observer_2 = jasmine.createSpy();
+            const observer_3 = jasmine.createSpy();
+            const parameters = [ uuid(), uuid(), uuid() ];
+
+            observable.register( observer_1, observer_2, observer_3 );
+            observable.notify( ...<[]>parameters );
+
+            expect( observer_1 ).toHaveBeenCalledTimes( 1 );
+            expect( observer_2 ).toHaveBeenCalledTimes( 1 );
+            expect( observer_3 ).toHaveBeenCalledTimes( 1 );
         });
 
         it( 'returns #notify() parent Observable object', () =>
         {
-            //
+            const observable = new Observable();
+            const returned = observable.notify();
+
+            expect( returned ).toBe( observable );
         });
 
         it( 'calls all registered observers with "values" as spread parameters', () =>
         {
-            //
+            const observable = new Observable();
+            const observer_1 = jasmine.createSpy();
+            const observer_2 = jasmine.createSpy();
+            const observer_3 = jasmine.createSpy();
+            const parameters = [ uuid(), uuid(), uuid() ];
+
+            observable.register( observer_1, observer_2, observer_3 );
+            observable.notify( ...<[]>parameters );
+
+            expect( observer_1 ).toHaveBeenCalledWith( ...parameters );
+            expect( observer_2 ).toHaveBeenCalledWith( ...parameters );
+            expect( observer_3 ).toHaveBeenCalledWith( ...parameters );
         });
 
     });
@@ -46,12 +89,28 @@ describe( 'Observable', () =>
 
         it( 'returns true if every provided "observers" are registered', () =>
         {
-            //
+            const observer_1 = () => {};
+            const observer_2 = () => {};
+            const observer_3 = () => {};
+            const observable = new Observable();
+
+            observable.register( observer_1, observer_2, observer_3 );
+
+            expect( observable.has( observer_1 ) ).toBe( true );
+            expect( observable.has( observer_1, observer_2, observer_3 ) ).toBe( true );
         });
 
         it( 'returns false if some provided "observers" are registered', () =>
         {
-            //
+            const observer_1 = () => {};
+            const observer_2 = () => {};
+            const observer_3 = () => {};
+            const observable = new Observable();
+
+            observable.register( observer_1, observer_2 );
+
+            expect( observable.has( observer_3 ) ).toBe( false );
+            expect( observable.has( observer_1, observer_2, observer_3 ) ).toBe( false );
         });
 
     });
@@ -61,12 +120,22 @@ describe( 'Observable', () =>
 
         it( 'registers "observers" to this observable', () =>
         {
-            //
+            const observer_1 = () => {};
+            const observer_2 = () => {};
+            const observable = new Observable();
+
+            observable.register( observer_1, observer_2 );
+
+            expect( observable.has( observer_1 ) ).toBe( true );
+            expect( observable.has( observer_2 ) ).toBe( true );
         });
 
         it( 'returns #register() parent Observable object', () =>
         {
-            //
+            const observable = new Observable();
+            const returned = observable.register();
+
+            expect( returned ).toBe( observable );
         });
 
     });
@@ -76,12 +145,23 @@ describe( 'Observable', () =>
 
         it( 'returns #unregister() parent Observable object', () =>
         {
-            //
+            const observable = new Observable();
+            const returned = observable.unregister();
+
+            expect( returned ).toBe( observable );
         });
 
         it( 'unregisters "observers" to this observable', () =>
         {
-            //
+            const observer_1 = () => {};
+            const observer_2 = () => {};
+            const observable = new Observable();
+
+            observable.register( observer_1, observer_2 );
+            observable.unregister( observer_1, observer_2 );
+
+            expect( observable.has( observer_1 ) ).toBe( false );
+            expect( observable.has( observer_2 ) ).toBe( false );
         });
 
     });
