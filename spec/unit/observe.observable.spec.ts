6f57466cb0d9b54e59e96b6af4ac445472aaa1e3
SalathielGenese <salathielgenese@gmail.com>
2019-01-01 08:02:11 +0000

    adds spec for Observable API

---
+++ spec/unit/observe.observable.spec.ts
@@ -0,0 +1,69 @@
+describe( 'Observable', () =>
+{
+
+    describe( '#reset()', () =>
+    {
+
+        it( 'clear registered observers', () =>
+        {
+            //
+        });
+
+    });
+
+    describe( '#notify()', () =>
+    {
+
+        it( 'calls all registered observers', () =>
+        {
+            //
+        });
+
+        it( 'calls all registered observers once', () =>
+        {
+            //
+        });
+
+        it( 'calls all registered observers with "values" as spread parameters', () =>
+        {
+            //
+        });
+
+    });
+
+    describe( '#has()', () =>
+    {
+
+        it( 'returns true if every provided "observers" are registered', () =>
+        {
+            //
+        });
+
+        it( 'returns false if some provided "observers" are registered', () =>
+        {
+            //
+        });
+
+    });
+
+    describe( '#register()', () =>
+    {
+
+        it( 'registers "observers" to this observable', () =>
+        {
+            //
+        });
+
+    });
+
+    describe( '#unregister()', () =>
+    {
+
+        it( 'unregisters "observers" to this observable', () =>
+        {
+            //
+        });
+
+    });
+
+});
