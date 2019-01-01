34895d4b7be1de825a72b7f856f1792689a1a5ef
SalathielGenese <salathielgenese@gmail.com>
2019-01-01 00:52:03 +0000

    defines Observe API

---
+++ src/observe.ts
@@ -0,0 +1,31 @@
+export class Observable<VV extends any[]>
+{
+    public reset(): this
+    {
+        throw new Error( 'method not yet implemented' );
+    }
+
+    public notify( ...values: VV ): this
+    {
+        throw new Error( 'method not yet implemented' );
+    }
+
+    public register( ...observers: ObserverLike<VV>[] ): this
+    {
+        throw new Error( 'method not yet implemented' );
+    }
+
+    public unregister( ...observers: ObserverLike<VV>[] ): this
+    {
+        throw new Error( 'method not yet implemented' );
+    }
+}
+
+export class StageObservable<VV extends any[]> extends Observable<VV>
+{
+}
+
+export interface ObserverLike<VV extends any[]>
+{
+    ( ...values: VV ): any;
+}
