faa0aa997b26942fc70229829fc0aa9ca7c10d62
SalathielGenese <salathielgenese@gmail.com>
2019-01-01 08:49:04 +0000

    implements Observable API

--- src/observe.ts
+++ src/observe.ts
@@ -1,28 +1,55 @@
 export class Observable<VV extends any[] = []>
 {
+    public constructor()
+    {
+        observables.set( this, new Set() );
+    }
+
     public reset(): this
     {
-        throw new Error( 'method not yet implemented' );
+        observables.get( this )!.clear();
+
+        return this;
     }
 
     public notify( ...values: VV ): this
     {
-        throw new Error( 'method not yet implemented' );
+        for ( let observer of Array.from( observables.get( this )!.values() ) )
+        {
+            observer( ...values );
+        }
+
+        return this;
     }
 
     public has( ...observers: ObserverLike<VV>[] ): boolean
     {
-        throw new Error( 'method not yet implemented' );
+        const zervers = observables.get( this )!;
+
+        return observers.length
+            ? observers.every( observer =>
+                zervers.has( <ObserverLike<any[]>>observer ) )
+            : !!zervers.size
     }
 
     public register( ...observers: ObserverLike<VV>[] ): this
     {
-        throw new Error( 'method not yet implemented' );
+        for ( let observer of observers )
+        {
+            observables.get( this )!.add( <ObserverLike<any[]>>observer );
+        }
+
+        return this;
     }
 
     public unregister( ...observers: ObserverLike<VV>[] ): this
     {
-        throw new Error( 'method not yet implemented' );
+        for ( let observer of observers )
+        {
+            observables.get( this )!.delete( <ObserverLike<any[]>>observer );
+        }
+
+        return this;
     }
 }
 
@@ -34,3 +61,5 @@ export interface ObserverLike<VV extends any[]>
 {
     ( ...values: VV ): any;
 }
+
+const observables = new WeakMap<Observable<any[]>, Set<ObserverLike<any[]>>>();
