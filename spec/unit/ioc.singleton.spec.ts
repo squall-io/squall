6b86ca41a38788132f1a0feea4da08483363a928
SalathielGenese <salathielgenese@gmail.com>
2019-02-11 06:55:11 +0100

    specs @Singleton( true )

--- spec/unit/ioc.singleton.spec.ts
+++ spec/unit/ioc.singleton.spec.ts
@@ -29,6 +29,30 @@ describe( '@Singleton', () =>
 
     });
 
+    describe( '( true )', () =>
+    {
+
+        it( 'restrict a single instance of the class it is applied on', () =>
+        {
+            @Singleton( false )
+            class Stdin {}
+
+            expect( () => new Stdin() ).not.toThrow();
+            expect( () => new Stdin() ).toThrow();
+        });
+
+        it( 'restrict a single instance of the class it is applied on accounting descendent instances', () =>
+        {
+            @Singleton( false )
+            class Stdin {}
+            class SshStdin extends Stdin {};
+
+            expect( () => new Stdin() ).not.toThrow();
+            expect( () => new SshStdin() ).toThrow();
+        });
+
+    });
+
     describe( '( false )', () =>
     {
 
