e25279374f072f43a78a5dfb57372735024f4fb6
SalathielGenese <salathielgenese@gmail.com>
2019-02-11 06:39:17 +0100

    nest spec into no-arg describe block

--- spec/unit/ioc.singleton.spec.ts
+++ spec/unit/ioc.singleton.spec.ts
@@ -5,13 +5,18 @@ import { Singleton } from '../../src/ioc';
 describe( '@Singleton', () =>
 {
 
-    it( 'restrict a single instance of the class it is applied on', () =>
+    describe( '()', () =>
     {
-        @Singleton()
-        class Stdin {}
 
-        expect( () => new Stdin() ).not.toThrow();
-        expect( () => new Stdin() ).toThrow();
+        it( 'restrict a single instance of the class it is applied on', () =>
+        {
+            @Singleton()
+            class Stdin {}
+
+            expect( () => new Stdin() ).not.toThrow();
+            expect( () => new Stdin() ).toThrow();
+        });
+
     });
 
 });
