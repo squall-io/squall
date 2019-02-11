51a8043c2c755ef2f4472d778b7150d4b833e529
SalathielGenese <salathielgenese@gmail.com>
2019-02-11 06:42:08 +0100

    spec @Singleton() descending classes instanciation

--- spec/unit/ioc.singleton.spec.ts
+++ spec/unit/ioc.singleton.spec.ts
@@ -17,6 +17,16 @@ describe( '@Singleton', () =>
             expect( () => new Stdin() ).toThrow();
         });
 
+        it( 'restrict a single instance of the class it is applied on accounting descendent instances', () =>
+        {
+            @Singleton()
+            class Stdin {}
+            class SshStdin extends Stdin {};
+
+            expect( () => new Stdin() ).not.toThrow();
+            expect( () => new SshStdin() ).toThrow();
+        });
+
     });
 
 });
