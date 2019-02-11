84df5f7bfa7ef404cba59ea9a7ae9ab9b69bd323
SalathielGenese <salathielgenese@gmail.com>
2019-02-11 06:35:16 +0100

    uncomments spec

--- spec/unit/ioc.singleton.spec.ts
+++ spec/unit/ioc.singleton.spec.ts
@@ -1,17 +1,17 @@
-// import { Singleton } from '../../src/ioc';
+import { Singleton } from '../../src/ioc';
 
 
 
-// describe( '@Singleton', () =>
-// {
+describe( '@Singleton', () =>
+{
 
-//     it( 'restrict a single instance of the class it is applied on', () =>
-//     {
-//         @Singleton()
-//         class Stdin {}
+    it( 'restrict a single instance of the class it is applied on', () =>
+    {
+        @Singleton()
+        class Stdin {}
 
-//         expect( () => new Stdin() ).not.toThrow();
-//         expect( () => new Stdin() ).toThrow();
-//     });
+        expect( () => new Stdin() ).not.toThrow();
+        expect( () => new Stdin() ).toThrow();
+    });
 
-// });
+});
