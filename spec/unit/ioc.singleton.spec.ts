fa96d18cf86a8aa7961c40b703cafdad76700d65
SalathielGenese <salathielgenese@gmail.com>
2019-02-20 05:31:31 +0100

    FIX specs, inverting assertions to match decorator description

--- spec/unit/ioc.singleton.spec.ts
+++ spec/unit/ioc.singleton.spec.ts
@@ -96,7 +96,7 @@ describe( '@Singleton', () =>
                         class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
@@ -113,7 +113,7 @@ describe( '@Singleton', () =>
                         @Singleton( false ) class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
@@ -130,7 +130,7 @@ describe( '@Singleton', () =>
                         @Singleton( true ) class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
@@ -153,7 +153,7 @@ describe( '@Singleton', () =>
                         class A1 extends A {}
 
                         A1;
-                    }).toThrow();
+                    }).not.toThrow();
                 });
 
             });
@@ -170,7 +170,7 @@ describe( '@Singleton', () =>
                         @Singleton( false ) class A1 extends A {}
 
                         A1;
-                    }).toThrow();
+                    }).not.toThrow();
                 });
 
             });
@@ -187,7 +187,7 @@ describe( '@Singleton', () =>
                         @Singleton( true ) class A1 extends A {}
 
                         A1;
-                    }).toThrow();
+                    }).not.toThrow();
                 });
 
             });
@@ -210,7 +210,7 @@ describe( '@Singleton', () =>
                         class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
@@ -227,7 +227,7 @@ describe( '@Singleton', () =>
                         @Singleton( false ) class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
@@ -244,7 +244,7 @@ describe( '@Singleton', () =>
                         @Singleton( true ) class A1 extends A {}
 
                         A1;
-                    }).not.toThrow();
+                    }).toThrow();
                 });
 
             });
