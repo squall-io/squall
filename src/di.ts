93338f27bed585433b6965ad69ecc04c4bd26497
SalathielGenese <salathielgenese@gmail.com>
2019-05-22 01:02:07 +0100

    Document the Injector .get instance method

--- src/di.ts
+++ src/di.ts
@@ -1,5 +1,12 @@
 export class Injector
 {
+    /**
+     *
+     * Get dependency from injector
+     *
+     * @param param0 dependency setup
+     *
+     */
     public get<T>({ id }: { id: string }): T;
     public get<T>({ type }: { type: ConstructorLike<T> }): T;
     public get<T>({ id, type }: { id: string, type: ConstructorLike<T> }): T;
