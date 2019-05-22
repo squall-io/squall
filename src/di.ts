00b122f3fe671b3552943d644c79ee089b1cad6e
SalathielGenese <salathielgenese@gmail.com>
2019-05-22 02:46:08 +0100

    Interface Inject .set instance method

--- src/di.ts
+++ src/di.ts
@@ -14,4 +14,19 @@ export class Injector
     {
         throw new Error( 'Not yet implemented' );
     }
+
+    /**
+     *
+     * Get dependency from injector
+     *
+     * @param param0 dependency setup
+     *
+     */
+    public set<T>({ id, value }: { id: string, value: T }): T;
+    public set<T>({ type, value }: { type: ConstructorLike<T>, value: T }): T;
+    public set<T>({ id, type, value }: { id: string, type: ConstructorLike<T>, value: T }): T;
+    public set<T>({ id, type, value }: { id?: string, type?: ConstructorLike<T>, value: T }): T
+    {
+        throw new Error( 'Not yet implemented' );
+    }
 }
